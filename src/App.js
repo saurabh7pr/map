601fd49b4b645da8f78a279c1323dd0f9ef6aaa8
saurabh7pr <saurabh7pr@gmail.com>
2024-08-12 00:52:07 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import D3MapCopy from './D3MapCopy';
+
+function App() {
+  return (
+    <div className="App">
+      <D3MapCopy/>
+    </div>
+  );
+}
+
+export default App;
